3396448c456eed83cfe509f47b5b05fe8815718e
jjasmin <john.jasmin@cambridge.org>
2024-09-21 20:58:26 +0800

    Added metadata of website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,34 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Head from 'next/head'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Phendex Medical'
-}
+  title: "Phendex Medical",
+  openGraph: {
+    type: 'website',
+    url: 'https://www.phendexmedical.net/',
+    siteName: 'Phendex Medical',
+    title: 'Phendex Medical',
+    description: 'Experience the Phendex Medical Trading Inc. difference today.',
+    images: [
+      {
+        url: '/Phendex_Logo.PNG',
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
